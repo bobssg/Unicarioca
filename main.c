0089482e9dd968c1698e0e95f4b43ad562058a58
bobssg <bobssg@gmail.com>
2022-09-03 07:36:54 -0300

    tema1ex1

--- main.c
+++ main.c
@@ -13,20 +13,20 @@
 //    else
 //        printf("\nReprovado");
 
-
-
 int main() {
-    printf("Hello, World!\n");
+    int cont;
+    float media_final;
+    char nome [20];
+
+    for (cont = 1; cont <= 5 ; cont++) {
+        printf("\n\nDigite o nome do aluno: <<");
+        scanf("%s", nome);
+        printf("\Digite a media final do aluno: <<");
+        scanf("%f",&media_final);
+        if (media_final >= 7)
+            printf("\nAprovado!");
+        else
+            printf("\nReprovado");
+    }
     return 0;
-}
-int main() {
-    printf("Digita o nome do launo: <<");
-    scanf("%s", nome);
-    printf("\Digite a media final do aluno: <<");
-    scanf("%f",&media_final);
-    if(media_final >= 7)
-        printf("\nAprovado!");
-    else
-        printf("\nReprovado");
-
 }
