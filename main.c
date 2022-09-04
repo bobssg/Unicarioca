4e32f6b20ff27b9417273e5b69f66ac00b34e1ad
bobssg <bobssg@gmail.com>
2022-09-04 16:22:10 -0300

    tema1ex2

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 
 //Exercício 1
 //Faça um código para contar o número de vezes que a letra “a” aparece em uma palavra
@@ -12,21 +14,21 @@
 int main(){
     char palavra [50];
     int qtdAs = 0;
-    int i;
+    int lente, i;
 
     printf("Digite a palavra: ");
     scanf("%s", palavra);
-    printf("\n\n palavra digitada:\n %s", palavra);
 
-    for (i=0; palavra[i] !='\0'; i=i+1) {
+    lente = strlen(palavra);
 
-        if (palavra[i] == 'a' || palavra[i] == 'A');
-        qtdAs = qtdAs + 1;
+    for (i=0; i <= lente; i++) {
 
+        if (palavra[i] == 'a' || palavra[i] == 'A'){
+
+            qtdAs++;
+        }
     }
-    printf("A quantidade de letas a sao; %i", qtdAs);
-
-
+    printf("A quantidade de letas a sao; %d", qtdAs);
 
     return 0;
 
--- tema1ex2.h
+++ tema1ex2.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 
 //Exercício 1
 //Faça um código para contar o número de vezes que a letra “a” aparece em uma palavra
@@ -11,22 +13,22 @@
 
 int main(){
     char palavra [50];
-    int qtdLetras , qtdAs = 0, i;
+    int qtdAs = 0;
+    int lente, i;
 
     printf("Digite a palavra: ");
-    scanf("s", &palavra);
+    scanf("%s", palavra);
 
-    qtdLetras = strlen(palavra);
+    lente = strlen(palavra);
 
-    for (i = 0; i<= qtdLetras; i++) {
-        switch (palavra[i]) {
-            case 'A':
-            case 'a':
-                qtdAs++;
-                break;
+    for (i=0; i <= lente; i++) {
+
+        if (palavra[i] == 'a' || palavra[i] == 'A'){
+
+            qtdAs++;
         }
     }
-
+    printf("A quantidade de letas a sao; %d", qtdAs);
 
     return 0;
 
